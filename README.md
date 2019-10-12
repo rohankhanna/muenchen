62a0403e10f6f334bf91f8690670bc76d6885db2
Aleksei Dmitriev <alex.i.dmitriev@gmail.com>
2019-10-12 20:18:19 +0200

    Add script for Blue Card EU appointments

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # muenchen
 Useful tools for newcomers in Munich
+
+## [blauekarte.py](blauekarte.py)
+Recently it became hard to get an appointment for Blue Card EU in Munich.
+This script is intended for monitoring available appointments. Optionally, you
+can implement your own alarm function (e.g. send an SMS to mobile phone). By
+default, the script checks for available appointments every 120 seconds,
+assuming that you need an appointment for a single person. This could be changed
+by passing different values to the TerminChecker class constructor (see class
+declaration).
---
+++ blauekarte.py
@@ -0,0 +1,71 @@
+from time import sleep
+import requests
+import re
+import json
+from datetime import datetime
+
+class TerminChecker:
+    def __init__(self, interval = 120, numberOfPersons = 1):
+        self.interval = interval
+        self.numberOfPersons = numberOfPersons
+        self.shouldAlarm = True
+        
+    def run(self):
+        while True:
+            self.checkTermin()
+            sleep(self.interval)
+
+    def checkTermin(self):
+        self.session = requests.Session()
+        page = self.loadPage(self.getNCFormInfo())
+        termins = self.getTermins(page)
+        self.processTermins(termins)
+        
+    def processTermins(self,termins):
+        existingTermins = {k: v for k, v in termins.items() if v}
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        if existingTermins:
+            print (timestamp, "Appointment found!!!")
+            for date, termins in existingTermins.items():
+                print (date, termins)
+            if self.shouldAlarm:
+                self.alarm()
+                self.shouldAlarm = False
+        else:
+            print (timestamp, "No appointment")
+            self.shouldAlarm = True
+                
+    def alarm(self):
+        # TODO: implement your own alarm function
+        # Hint: I used smsflatrate.net service, which allows sending an SMS via HTTP GET request
+        return
+
+    def getNCFormInfo(self):
+        page = self.loadPage()
+        m = re.search("<input type=\"hidden\" name=\"__ncforminfo\" value=\"(.*)\"/>", page)
+        if (m):
+            return m.group(1)
+    
+    def loadPage(self, ncforminfo = None):
+        endpoint = "https://www46.muenchen.de/termin/index.php?cts=1080627"
+
+        if (ncforminfo):
+            data = {"step": "WEB_APPOINT_SEARCH_BY_CASETYPES",
+                    "CASETYPES[Aufenthaltserlaubnis Blaue Karte EU]": str(self.numberOfPersons),
+                    "__ncforminfo": ncforminfo}
+            r = self.session.post(url = endpoint, data = data)
+        else:
+            r = self.session.get(url = endpoint)
+
+        return r.text
+
+    def getTermins(self, page):
+        m = re.search("var jsonAppoints = '(.*)';", page)
+        if (m):
+            return json.loads(m.group(1)).popitem()[1]['appoints']
+
+if ( __name__ == "__main__"):
+   tc = TerminChecker()
+   tc.run()
+
