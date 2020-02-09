00ba88614efe96bf0d14190d60224f00dc47f6f2
Rohan Khanna <rohan.kh@gmail.com>
2020-02-09 21:07:33 +0100

    Update blauekarte.py

--- blauekarte.py
+++ blauekarte.py
@@ -7,8 +7,8 @@ import requests
 
 def telegram_bot_sendtext(bot_message):
     
-    bot_token = '1001724290:AAErldTxnrzl1BHcfvSatuJkdKbR3f3Zrt0'
-    bot_chatID = '720456025'
+    bot_token = ''
+    bot_chatID = ''
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
